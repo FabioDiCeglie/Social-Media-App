c22f88ad6a9a2c8992ec6ebd8e78972cafa5ff97
FabioDiCeglie <dcfabio96@gmail.com>
2023-03-31 14:10:33 +0200

    feat: create express server

---
+++ server/index.ts
@@ -0,0 +1,65 @@
+import express, { json } from "express";
+import bodyParser, { urlencoded } from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet, { crossOriginResourcePolicy } from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+
+app.use(json());
+
+// This line of code is a security middleware that helps protect the application
+// from common vulnerabilities such as cross-site scripting (XSS), content sniffing, clickjacking, etc.
+app.use(helmet());
+
+// This line of code sets a Cross-Origin Resource Policy (CORP) header to allow cross-origin requests.
+// This middleware helps to prevent attackers
+// from exploiting vulnerabilities in the application by sending malicious requests from other domains.
+app.use(crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// This line of code is a logging middleware that logs HTTP requests and responses
+app.use(morgan("common"));
+
+// This line of code sets up a middleware that parses JSON data in incoming requests and rejects any request that does not
+// conform to the JSON syntax. It also limits the request body size to 30MB.
+app.use(bodyParser.json({ limit: "30mb", strict: true }));
+
+// This line of code sets up a middleware that parses URL-encoded data in incoming requests and makes it available in
+// the req.body property of the request object.
+// It also limits the request body size to 30MB and allows nested objects in the URL-encoded data.
+app.use(urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// Set up file storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+const port = 3001 || 6001;
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() => {
+    app.listen(port, () =>
+      console.log(`Server started: http://localhost:${port}`)
+    );
+  })
+  .catch((error) => console.log(`${error} did not connect`));
