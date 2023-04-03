538613aba79ce9ced926da7048d04478c17ad6e1
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-03 21:43:02 +0200

    feat: implement get user function

--- server/controllers/users.ts
+++ server/controllers/users.ts
@@ -1,7 +1,21 @@
 import { Response, Request } from "express";
+import { User } from "../models/User";
 
-export const getUser = (req: Request, res: Response) => {};
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
 
-export const getUserFriends = (req: Request, res: Response) => {};
+    const user = await User.findById(id);
 
-export const addRemoveFriend = (req: Request, res: Response) => {};
+    if (!user)
+      return res.status(400).json({ msg: `User: ${user} does not exist` });
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ error: (err as Error).message });
+  }
+};
+
+export const getUserFriends = async (req: Request, res: Response) => {};
+
+export const addRemoveFriend = async (req: Request, res: Response) => {};
