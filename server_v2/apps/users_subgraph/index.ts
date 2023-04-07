fc936d9bec11a9bcc0638f711a71c7beb5a7aafe
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-07 16:33:55 +0200

    fix: server

--- server_v2/apps/users_subgraph/index.ts
+++ server_v2/apps/users_subgraph/index.ts
@@ -1,15 +1,37 @@
 import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
 import { json } from "express";
-import multer from "multer";
 import morgan from "morgan";
 import { app, httpServer, server } from "./server";
 import { register } from "./rest";
+import multer from "multer";
+import helmet, { crossOriginResourcePolicy } from "helmet";
+import bodyParser, { urlencoded } from "body-parser";
+import mongoose from "mongoose";
+require("dotenv").config();
 
-app.use(
-  morgan(":method :url :status :response-time ms - :res[content-length]")
-);
+// This line of code is a logging middleware that logs HTTP requests and responses
+app.use(morgan("common"));
 app.use(cors());
+app.use(json());
+
+// This line of code is a security middleware that helps protect the application
+// from common vulnerabilities such as cross-site scripting (XSS), content sniffing, clickjacking, etc.
+app.use(helmet());
+
+// This line of code sets a Cross-Origin Resource Policy (CORP) header to allow cross-origin requests.
+// This middleware helps to prevent attackers
+// from exploiting vulnerabilities in the application by sending malicious requests from other domains.
+app.use(crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// This line of code sets up a middleware that parses JSON data in incoming requests and rejects any request that does not
+// conform to the JSON syntax. It also limits the request body size to 30MB.
+app.use(bodyParser.json({ limit: "30mb", strict: true }));
+
+// This line of code sets up a middleware that parses URL-encoded data in incoming requests and makes it available in
+// the req.body property of the request object.
+// It also limits the request body size to 30MB and allows nested objects in the URL-encoded data.
+app.use(urlencoded({ limit: "30mb", extended: true }));
 
 // Set up file storage
 const storage = multer.diskStorage({
@@ -38,9 +60,17 @@ const startApolloServer = async () => {
   const port = 4004;
   await new Promise<void>((resolve, reject) => {
     httpServer.on("error", reject);
-    httpServer.listen({ port }, resolve);
+    mongoose
+      .connect(process.env.MONGO_URL as string)
+      .then(() => {
+        httpServer.listen({ port }, resolve);
+      })
+      .catch((error) => console.log(`${error} did not connect`));
   });
   console.info(`🚀 Server ready at http://localhost:${port}/graphql`);
 };
 
-startApolloServer();
+startApolloServer().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
