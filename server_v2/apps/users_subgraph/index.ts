576ad40fa4724e830c3bc54fd549a17fc33b7355
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-07 16:22:10 +0200

    feat: create local storage

--- server_v2/apps/users_subgraph/index.ts
+++ server_v2/apps/users_subgraph/index.ts
@@ -1,13 +1,30 @@
 import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
 import { json } from "express";
+import multer from "multer";
 import morgan from "morgan";
 import { app, httpServer, server } from "./server";
+import { register } from "./rest";
 
 app.use(
   morgan(":method :url :status :response-time ms - :res[content-length]")
 );
 app.use(cors());
+
+// Set up file storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// Routes with files
+app.post("/auth/register", upload.single("picture"), register);
 
 const startApolloServer = async () => {
   await server.start();
