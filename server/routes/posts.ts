ff777b6974a74bcd2e380d8782565d374a88179e
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-07 15:03:57 +0200

    feat: creating routes for posts

---
+++ server/routes/posts.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { verifyToken } from "../middleware/auth";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts";
+
+const router = Router();
+
+// READ
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+// UPDATE
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
