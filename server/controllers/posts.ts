9066cc2ba1792b84a6534b53ae76f7808e75af26
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-07 15:03:42 +0200

    chore: start working on controllers

---
+++ server/controllers/posts.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export const createPost = (req: Request, res: Response) => {
+  try {
+  } catch (err) {}
+};
+
+export const getFeedPosts = (req: Request, res: Response) => {
+  try {
+  } catch (err) {}
+};
+
+export const getUserPosts = (req: Request, res: Response) => {
+  try {
+  } catch (err) {}
+};
+
+export const likePost = (req: Request, res: Response) => {
+  try {
+  } catch (err) {}
+};
