b82785ab2c55f8f5c9f998dd588a10adad92d6f4
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-07 16:04:07 +0200

    feat: create typeDefs

---
+++ server_v2/apps/users_subgraph/typeDefs.ts
@@ -0,0 +1,36 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+
+  type Friend {
+  id: String!
+  firstName: String!
+  lastName: String!
+  occupation: String;
+  location: String;
+  picturePath: String;
+  }
+
+  type User {
+    id: String!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+    picturePath: String
+    friends: [Friend]
+    location: String
+    occupation: String
+    viewedProfile: String
+    impressions: Number
+  }
+
+  type Query {
+    user(id: String): [User]
+    userFriends(id: String): [Friend]
+  }
+
+  type Mutation {
+    addRemoveFriend(id:String, friendId: String): [Friend]
+  }
+`;
