63ff36ad0fd4140b8f17eb90a7a0ab0efd515846
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-13 15:13:33 +0200

    feat: include rubik font

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Rubik } from 'next/font/google'
 
+const rubik = Rubik({ weight: ['400', '500', '700'] })
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <main className={rubik.className}>
+    <Component {...pageProps} />
+  </main>
+  )
 }
