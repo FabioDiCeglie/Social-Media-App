f6818a84d71a1296cea8b8c0ca139798936a455c
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-14 16:18:29 +0200

    feat: add types for user

---
+++ client/lib/types.ts
@@ -0,0 +1,23 @@
+export type Friend = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    occupation: string;
+    location: string;
+    picturePath: string;
+};
+
+export type IUser = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    occupation: string;
+    location: string;
+    picturePath: string;
+    impressions: number;
+    viewedProfile: number;
+    token?: string;
+    friends: Friend[];
+};
