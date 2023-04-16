b8610931b62d6d03664209d9070cfa1ddb2a06f6
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-16 19:01:02 +0200

    feat: use wrapper for redux

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,14 +3,30 @@ import type { AppProps } from 'next/app'
 import { Rubik } from 'next/font/google'
 import { Provider } from "react-redux";
 import store from "../../state";
+import { useMemo} from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "../../theme";
+import { createWrapper } from "next-redux-wrapper";
 
 const rubik = Rubik({ weight: ['400', '500', '700'] , subsets: ['cyrillic'] })
-export default function App({ Component, pageProps }: AppProps) {
+const wrapper = createWrapper(() => store);
+
+function App({ Component, pageProps }: AppProps) {
+  const mode = useSelector((state: {mode: string}) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
       <Provider store={store}>
-        <main className={rubik.className}>
-            <Component {...pageProps} />
-        </main>
+          <ThemeProvider theme={theme}>
+              <CssBaseline />
+                <main className={rubik.className}>
+                    <Component {...pageProps} />
+                </main>
+          </ThemeProvider>
       </Provider>
   )
 }
+
+export default wrapper.withRedux(App);
