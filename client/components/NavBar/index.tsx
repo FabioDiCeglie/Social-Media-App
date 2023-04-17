a7c734c82ddd86298ff4e400a080421a2f59f2e1
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-17 22:14:49 +0200

    chore: continuing working to the NavBar

--- client/components/NavBar/index.tsx
+++ client/components/NavBar/index.tsx
@@ -1,31 +1,48 @@
-import {  useState  } from 'react'
-import { Box, IconButton, InputBase, Typography, Select, MenuItem, FormControl, useTheme, useMediaQuery } from '@mui/material'
-import { Search, Message, DarkMode, LightMode, Notifications, Help, Menu, Close } from "@mui/icons-material";
+import { useState } from "react";
+import { Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import { setMode, setLogout } from "../../state";
-import Link from 'next/link';
 import FlexBetween from "../FlexBetween";
-import {IPalette, IUser} from "../../lib/types";
+import { IPalette, IUser } from "../../lib/types";
+import { useRouter } from "next/router";
 
 const NavBar = () => {
-    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false)
-    const dispatch = useDispatch()
-    const user = useSelector((state: {user: IUser}) => state.user)
-    const isNonMobileScreen = useMediaQuery("(min-width: 1000px)")
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const user = useSelector((state: { user: IUser }) => state.user);
+  const router = useRouter();
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
 
-    const theme = useTheme()
-    const palette = theme.palette as unknown as IPalette
-    const { neutral, background: backgroundPalette, primary } = palette
-    const neutralLight = neutral.light
-    const dark = neutral.dark
-    const background = backgroundPalette.default;
-    const primaryLight = primary.light;
-    const alt = backgroundPalette.alt;
-    return (
-        <>
-            <h1>Ciao me</h1>
-    </>
-)
-}
+  const theme = useTheme();
+  const palette = theme.palette as unknown as IPalette;
+  const { neutral, background: backgroundPalette, primary } = palette;
+  const neutralLight = neutral.light;
+  const dark = neutral.dark;
+  const background = backgroundPalette.default;
+  const primaryLight = primary.light;
+  const alt = backgroundPalette.alt;
 
-export default NavBar
+  // const fullName = `${user.firstName} ${user.lastName}`;
+
+  return (
+    <FlexBetween padding="1rem 6%" bgcolor={alt}>
+      <FlexBetween gap="1.75rem">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 2rem, 2.25rem)"
+          color="primary"
+          onClick={() => router.push("/")}
+          sx={{
+            "&:hover": {
+              color: primaryLight,
+              cursor: "pointer",
+            },
+          }}
+        >
+          Sociopedia
+        </Typography>
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
+
+export default NavBar;
