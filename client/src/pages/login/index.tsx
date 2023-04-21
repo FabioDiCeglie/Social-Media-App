6c5b774e470f5d6c4a326b63762528698f03af22
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-21 14:39:03 +0200

    feat: start working to login page

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,8 +1,30 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { IPalette } from "../../../lib/types";
+
 const Login = () => {
+    const theme = useTheme();
+    const palette = theme.palette as unknown as IPalette;
+    const { background } = palette;
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
     return (
-        <>
-            <h1>Ciao login</h1>
-        </>
+        <Box>
+            <Box width="100%" bgcolor={background.alt} p="1rem 6%" textAlign="center">
+                <Typography
+                    fontWeight="bold"
+                    fontSize="32px"
+                    color="primary"
+                >
+                    Sociogram
+                </Typography>
+                <Box width={isNonMobileScreens ? "50%" : "93%"}
+                    p="2rem" m="rem auto" borderRadius="1.5rem" bgcolor={background.alt}
+                >
+                    <Typography fontWeight="500" variant="h5" sx={{ mb:"1.5rem" }}>
+                        Login
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
