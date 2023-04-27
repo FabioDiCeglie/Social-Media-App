ea5511800fc44388e7174e4fe1e187d6dedc9455
FabioDiCeglie <dcfabio96@gmail.com>
2023-04-27 10:01:01 +0200

    chore: clean up some code

--- client/lib/types.ts
+++ client/lib/types.ts
@@ -1,76 +1,75 @@
-import { PaletteMode } from "@mui/material";
 import { PaletteColor } from "@mui/material/styles/createPalette";
 
 export type Friend = {
-    id: string;
-    firstName: string;
-    lastName: string;
-    occupation: string;
-    location: string;
-    picturePath: string;
+  id: string;
+  firstName: string;
+  lastName: string;
+  occupation: string;
+  location: string;
+  picturePath: string;
 };
 
 export type IUser = {
-    id: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    occupation: string;
-    location: string;
-    picturePath: string;
-    impressions: number;
-    viewedProfile: number;
-    token?: string;
-    friends: Friend[];
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  occupation: string;
+  location: string;
+  picturePath: string;
+  impressions: number;
+  viewedProfile: number;
+  token?: string;
+  friends: Friend[];
 };
 
 export type IPost = {
-    id: string
-    userId: string
-    firstName: string
-    lastName: string
-    location: string
-    description: string
-    picturePath: string
-    userPicturePath: string
-    likes: [ Like ]
-    comments: [ string ]
-}
+  id: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  location: string;
+  description: string;
+  picturePath: string;
+  userPicturePath: string;
+  likes: [Like];
+  comments: [string];
+};
 
 export type Like = {
-    userId: boolean
-}
+  userId: boolean;
+};
 
 export type IPalette = {
-    mode: string;
-    primary: PaletteColor;
-    neutral: {
-        dark: string;
-        main: string;
-        mediumMain: string;
-        medium: string;
-        light: string;
-    }
-    background: {
-        default: string;
-        alt: string;
-    }
+  mode: string;
+  primary: PaletteColor;
+  neutral: {
+    dark: string;
+    main: string;
+    mediumMain: string;
+    medium: string;
+    light: string;
+  };
+  background: {
+    default: string;
+    alt: string;
+  };
 };
 
 export type IRegister = {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    location: string;
-    occupation: string;
-    picture: {
-        name: string;
-    }
-}
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  location: string;
+  occupation: string;
+  picture: {
+    name: string;
+  };
+};
 
 export type ILogin = {
-    email: string;
-    password: string;
-}
+  email: string;
+  password: string;
+};
