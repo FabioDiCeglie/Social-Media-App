c6d7f9a555400ee36399398643a91fd8ee00f10a
FabioDiCeglie <dcfabio96@gmail.com>
2023-05-04 21:57:44 +0200

    feat: created PostWidget component

---
+++ client/components/PostWidget/index.tsx
@@ -0,0 +1,120 @@
+import { useMutation } from "@apollo/client";
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { LIKE_POST } from "../../lib/mutation";
+import { ITheme, Like } from "../../lib/types";
+import FlexBetween from "../FlexBetween";
+// import Friend from "../Friend";
+import WidgetWrapper from "../WidgetWrapper";
+
+type PostWidgetProps = {
+  key: string;
+  postId: string;
+  postUserId: string;
+  name: string;
+  description: string;
+  location: string;
+  picturePath: string;
+  userPicturePath: string;
+  likes: [Like];
+  comments: [string];
+};
+
+const PostWidget = ({
+  key,
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+  comments,
+}: PostWidgetProps) => {
+  const [likePost, { loading, error, data }] = useMutation(LIKE_POST, {
+    variables: { id: postId, userId: postUserId },
+  });
+
+  const [isComments, setIsComments] = useState(false);
+  const loggedInUserId = useSelector(
+    (state: { user: { id: string } }) => state.user.id
+  );
+  const isLiked = Boolean(likes[loggedInUserId as unknown as any]);
+  const likeCount = Object.keys(likes).length;
+
+  const { palette }: ITheme = useTheme();
+  const main = palette.neutral.main;
+  const primary = palette.primary.main;
+
+  return (
+    // @ts-ignore
+    <WidgetWrapper m="2rem 0">
+      {/* <Friend
+        friendId={postUserId}
+        name={name}
+        subtitle={location}
+        userPicturePath={userPicturePath}
+      /> */}
+      <Typography color={main} sx={{ mt: "1rem" }}>
+        {description}
+      </Typography>
+      {picturePath && (
+        <img
+          width="100%"
+          height="auto"
+          alt="post"
+          style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+          src={`http://localhost:3001/assets/${picturePath}`}
+        />
+      )}
+      <FlexBetween mt="0.25rem">
+        <FlexBetween gap="1rem">
+          <FlexBetween gap="0.3rem">
+            <IconButton onClick={() => likePost}>
+              {isLiked ? (
+                <FavoriteOutlined sx={{ color: primary }} />
+              ) : (
+                <FavoriteBorderOutlined />
+              )}
+            </IconButton>
+            <Typography>{likeCount}</Typography>
+          </FlexBetween>
+
+          <FlexBetween gap="0.3rem">
+            <IconButton onClick={() => setIsComments(!isComments)}>
+              <ChatBubbleOutlineOutlined />
+            </IconButton>
+            <Typography>{comments.length}</Typography>
+          </FlexBetween>
+        </FlexBetween>
+
+        <IconButton>
+          <ShareOutlined />
+        </IconButton>
+      </FlexBetween>
+      {isComments && (
+        <Box mt="0.5rem">
+          {comments.map((comment, i) => (
+            <Box key={`${name}-${i}`}>
+              <Divider />
+              <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                {comment}
+              </Typography>
+            </Box>
+          ))}
+          <Divider />
+        </Box>
+      )}
+    </WidgetWrapper>
+  );
+};
+
+export default PostWidget;
