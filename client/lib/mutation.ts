c488b22229fae7811b86c7ef25210f63f32b3ede
FabioDiCeglie <dcfabio96@gmail.com>
2023-05-04 21:57:28 +0200

    feat: create mutation likePost

--- client/lib/mutation.ts
+++ client/lib/mutation.ts
@@ -1,1 +1,20 @@
+import { gql } from "@apollo/client";
 
+export const LIKE_POST = gql`
+  mutation Mutation($likePostId: String!, $userId: String!) {
+    likePost(id: $likePostId, userId: $userId) {
+      description
+      firstName
+      id
+      lastName
+      likes {
+        userId
+      }
+      location
+      picturePath
+      userId
+      userPicturePath
+      comments
+    }
+  }
+`;
