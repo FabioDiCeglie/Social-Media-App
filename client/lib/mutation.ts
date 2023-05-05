bde4ad12858ef9168b9d210a84854beb6800e232
FabioDiCeglie <dcfabio96@gmail.com>
2023-05-05 12:16:32 +0200

    feat: created new mutation

--- client/lib/mutation.ts
+++ client/lib/mutation.ts
@@ -18,3 +18,16 @@ export const LIKE_POST = gql`
     }
   }
 `;
+
+export const ADD_REMOVE_FRIEND = gql`
+  mutation AddRemoveFriend($friendId: String, $userId: String) {
+    addRemoveFriend(friendId: $friendId, id: $userId) {
+      firstName
+      id
+      lastName
+      occupation
+      location
+      picturePath
+    }
+  }
+`;
